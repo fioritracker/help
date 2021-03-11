c7781482d80954a11c8487ae8038132ed8163727
gmalew <gmanype@gmail.com>
2021-03-11 15:30:44 +0100

    upd

--- sync.bat
+++ sync.bat
@@ -5,5 +5,4 @@ mkdocs build
 del /q "C:\vs\help-htm\fioritracker.github.io\V2020\*"
 FOR /D %%p IN ("C:\vs\help-htm\fioritracker.github.io\V2020\*.*") DO rmdir "%%p" /s /q
 rmdir "C:\vs\help-htm\fioritracker.github.io\V2020\" /s /q
-copy site C:\vs\help-htm\fioritracker.github.io\V2020
-
+xcopy site "C:\vs\help-htm\fioritracker.github.io\V2020" /h /i /c /k /e /r /y
