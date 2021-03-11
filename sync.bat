43c62ba2f7b10724e32f0d3666966c213db1a313
gmalew <gmanype@gmail.com>
2021-03-11 15:33:11 +0100

    upd

--- sync.bat
+++ sync.bat
@@ -6,3 +6,8 @@ del /q "C:\vs\help-htm\fioritracker.github.io\V2020\*"
 FOR /D %%p IN ("C:\vs\help-htm\fioritracker.github.io\V2020\*.*") DO rmdir "%%p" /s /q
 rmdir "C:\vs\help-htm\fioritracker.github.io\V2020\" /s /q
 xcopy site "C:\vs\help-htm\fioritracker.github.io\V2020" /h /i /c /k /e /r /y
+cd "C:\vs\help-htm\fioritracker.github.io\"
+git add -A
+git commit -am "upd"
+git push
+cd "C:\vs\help-src\ft\"
